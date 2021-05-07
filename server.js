ab038152eeab50e465885059c77ee1b2714e4dbc
Michelle <michey.asmah21@gmail.com>
2021-05-07 17:21:18 -0400

    fs write file

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 
@@ -10,15 +12,26 @@ app.use(express.json());
 
 const notes = require('./db/db');
 
+function createNewNotes (body, notesArray) {
+  const note = body;
+  notesArray.push(note)
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  // return finished code to post route for response
+  return note;
+}
+
 app.get('/api/notes', (req,res) => {
   res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
   req.body.id = uuidv4();
-  res.json(req.body);
+  const note = createNewNotes(req.body, notes)
+  res.json(note);
 });
-
 
 
 app.listen(PORT, () => {
