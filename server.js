92d0d1082fe8ab11f9c55918e25a70941c38a1c5
Michelle <michey.asmah21@gmail.com>
2021-05-07 16:51:48 -0400

    unique id

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
-const  notes = require('./db/db');
-
+const notes = require('./db/db');
 
 app.get('/api/notes', (req,res) => {
   res.json(notes);
 });
 
+app.post('/api/notes', (req, res) => {
+  req.body.id = uuidv4();
+  res.json(req.body);
+});
+
 
 
 app.listen(PORT, () => {
-  console.log(`API server now on port 3001!`);
+  console.log(`API server now on port ${PORT}!`);
 });
+
