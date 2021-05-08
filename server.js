9db5cae65654b53b6b4944da1922a4d1345c40e1
Michelle <michey.asmah21@gmail.com>
2021-05-07 20:46:43 -0400

    html

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const notes = require('./db/db');
 
 function createNewNotes (body, notesArray) {
   const note = body;
-  notesArray.push(note)
+  console.log(note)
+  notesArray.push(note);
   fs.writeFileSync(
     path.join(__dirname, './db/db.json'),
     JSON.stringify({ notes: notesArray }, null, 2)
@@ -37,6 +38,15 @@ app.post('/api/notes', (req, res) => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
