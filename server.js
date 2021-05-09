bcbb6d34072c59775d61268cf35f9b67f7ca72af
Michelle <michey.asmah21@gmail.com>
2021-05-08 20:33:35 -0400

    added comments

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
+//===============================================
+//Requirements
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+const notes = require('./db/db');
 
+//================================================
+//Express setup
 const PORT = process.env.PORT || 3001;
 const app = express();
-// parse incoming string or array data
+
+//=================================================
+//Middleware
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
 
-const notes = require('./db/db');
-
+//=======================================================
+//Push newly created notes into array and write to db.json
 function createNewNotes (body, notesArray) {
   const note = body;
   notesArray.push(note);
@@ -20,11 +26,11 @@ function createNewNotes (body, notesArray) {
     path.join(__dirname, './db/db.json'),
     JSON.stringify(notesArray, null, 2)
   );
-  
   return note;
 }
 
-
+//==========================================================
+//Routes serving API endpoints (POST,GET,DELETE)
 app.get('/api/notes', (req,res) => {
   res.json(notes);
 });
@@ -42,11 +48,11 @@ app.delete('/api/notes/:id', (req, res) => {
     path.join( __dirname, './db/db.json' ),
     JSON.stringify(notes, null, 1 )
   );
-
   res.json(notes)
-
 });
 
+//===========================================================
+//Routes serving HTML pages 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
@@ -59,6 +65,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+//===========================================================
+//PORT listener for server
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
